1a22f28e852dc534bacd53bbecd62639981f59db
Eduardo Lanchares <edulanchi@gmail.com>
2024-06-25 13:32:34 +0200

    Update index.js

--- api/index.js
+++ api/index.js
@@ -5,6 +5,7 @@ const { makeExecutableSchema } = require('@graphql-tools/schema')
 const typeDefs = /* GraphQL */ `
   type Order {
     id: ID!
+    merchantImage: String!
     merchantName: String!
     merchantLogo: String!
     date: String!
@@ -14,11 +15,12 @@ const typeDefs = /* GraphQL */ `
     reference: String!
     price: Float!
     numberOfArticles: Int!
-    shippedArticles: Int!    
+    shippedArticles: Int!
   }
 
   type Query {
     orders: [Order]
+    orderById(orderId: ID!): Order
   }
 
   type Mutation {
@@ -32,80 +34,89 @@ const typeDefs = /* GraphQL */ `
 `
 
 const orders = [
-    {
-        id: '1',
-        merchantName: 'Amazon',
-        merchantLogo: 'https://cdn.iconscout.com/icon/free/png-512/free-amazon-1543560-1306063.png',
-        date: '2021-10-01',
-        nextDueAmount: 100,
-        nextDueDate: '2021-11-01',
-        status: 'pending',
-        reference: '123456789',
-        price: 100,
-        numberOfArticles: 5,
-        shippedArticles: 2
-    },
-    {
-        id: '2',
-        merchantName: 'Ebay',
-        merchantLogo: 'https://cdn.iconscout.com/icon/free/png-512/free-ebay-13-675708.png',
-        date: '2021-10-01',
-        nextDueAmount: 200,
-        nextDueDate: '2021-11-01',
-        status: 'completed',
-        reference: '987654321',
-        price: 200,
-        numberOfArticles: 10,
-        shippedArticles: 5
-    },
-    {
-        id: '3',
-        merchantName: 'Costco',
-        merchantLogo: 'https://cdn.iconscout.com/icon/free/png-512/free-costco-282448.png',
-        date: '2023-05-30',
-        nextDueAmount: 300,
-        nextDueDate: '2024-11-01',
-        status: 'pending',
-        reference: '123456777',
-        price: 300,
-        numberOfArticles: 15,
-        shippedArticles: 7
-    }
+  {
+    id: '1',
+    merchantName: 'Amazon',
+    merchantImage:
+      'https://res.cloudinary.com/caskchain/image/upload/v1717158058/Sequra/amazon.png',
+    merchantLogo:
+      'https://cdn.iconscout.com/icon/free/png-512/free-amazon-1543560-1306063.png',
+    date: '2021-10-01',
+    nextDueAmount: 100,
+    nextDueDate: '2021-11-01',
+    status: 'pending',
+    reference: '123456789',
+    price: 100,
+    numberOfArticles: 5,
+    shippedArticles: 2,
+  },
+  {
+    id: '2',
+    merchantName: 'Ebay',
+    merchantImage:
+      'https://res.cloudinary.com/caskchain/image/upload/v1717158059/Sequra/ebay.png',
+    merchantLogo:
+      'https://cdn.iconscout.com/icon/free/png-512/free-ebay-13-675708.png',
+    date: '2021-10-01',
+    nextDueAmount: 200,
+    nextDueDate: '2021-11-01',
+    status: 'completed',
+    reference: '987654321',
+    price: 200,
+    numberOfArticles: 10,
+    shippedArticles: 5,
+  },
+  {
+    id: '3',
+    merchantName: 'Costco',
+    merchantImage:
+      'https://res.cloudinary.com/caskchain/image/upload/v1717158062/Sequra/costco.png',
+    merchantLogo:
+      'https://cdn.iconscout.com/icon/free/png-512/free-costco-282448.png',
+    date: '2023-05-30',
+    nextDueAmount: 300,
+    nextDueDate: '2024-11-01',
+    status: 'pending',
+    reference: '123456777',
+    price: 300,
+    numberOfArticles: 15,
+    shippedArticles: 7,
+  },
 ]
 
-
 const resolvers = {
-    Query: {
-        orders() {
-            return orders
-        }
+  Query: {
+    orders() {
+      return orders
     },
-    Mutation: {
-        payOrder(_, { orderId }) {
-            const order = orders.find(order =>  order.id === orderId)
-            if (!order) {
-                throw new Error(`Couldn't find order with id ${orderId}`)
-            }
+    orderById(_, { orderId }) {
+      return orders.find((order) => order.id === orderId) ?? null
+    },
+  },
+  Mutation: {
+    payOrder(_, { orderId }) {
+      const order = orders.find((order) => order.id === orderId)
+      if (!order) {
+        throw new Error(`Couldn't find order with id ${orderId}`)
+      }
 
-            order.status = 'completed'
-            return order
-        }
+      order.status = 'completed'
+      return order
     },
+  },
 }
 
-
 const executableSchema = makeExecutableSchema({
-    typeDefs,
-    resolvers
+  typeDefs,
+  resolvers,
 })
 
-
 const yoga = createYoga({
-    schema: executableSchema
+  schema: executableSchema,
 })
 
 const server = createServer(yoga)
 
 server.listen(4000, () => {
-    console.log('Yoga is listening at http://localhost:4000/graphql')
+  console.log('Yoga is listening at http://localhost:4000/graphql')
 })
