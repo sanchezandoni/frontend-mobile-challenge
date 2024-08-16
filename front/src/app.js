def138a9f271cc08e701434aca6b6da73463b750
sanchezandoni <andoni@berritu.com>
2024-08-16 13:06:01 +0200

    Actualiza la estructura básica de index para que comience a pintar el componente AllPurchases en la página

--- front/src/app.js
+++ front/src/app.js
@@ -1,1 +1,5 @@
-console.log('Hello world!');
+import AllPurchases from "./pages/AllPurchases";
+import { createRoot } from 'react-dom/client';
+
+const root = createRoot(document.getElementById('app'));
+root.render(<AllPurchases/>);
